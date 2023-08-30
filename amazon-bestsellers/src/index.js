452264da4c09db4de440b19245d2cc4f7041900a
lexibor <brionesornelas.alexia@gmail.com>
2023-08-30 12:05:54 -0500

    setting up the components

--- amazon-bestsellers/src/index.js
+++ amazon-bestsellers/src/index.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+function BookList()
+{
+    return (
+        <section>
+            <Book />
+        </section>
+    )
+}
+
+const Book = () =>
+{
+    return(
+        <article>
+            <Image />
+            <Title />
+            <Author />
+        </article>
+    )
+}
+
+const Image = () => <h2>Image Placeholder</h2>;
+const Title = () => <h2>Book Title</h2>;
+const Author = () => <h4>Author</h4>
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
