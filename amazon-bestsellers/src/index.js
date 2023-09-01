7ff980647891163dbb5676839bbe7dce45ef6da3
lexibor <brionesornelas.alexia@gmail.com>
2023-09-01 12:40:40 -0500

    props and image folder moved to public

--- amazon-bestsellers/src/index.js
+++ amazon-bestsellers/src/index.js
@@ -3,29 +3,35 @@ import ReactDOM from 'react-dom/client';
 
 import './index.css';
 
+const title = "Iron Flame (The Empyrean, 2)";
+const author = "Rebecca Yarros";
+const img = "./images/book-1.jpg";
+
 function BookList()
 {
     return (
         <section className='booklist'>
-            <Book />
-            <Book />
-            <Book />
-            <Book />
+            <Book job="developer" />
+            <Book title="random title" number={22}/>
         </section>
     )
 }
 
-const Book = () =>
+const Book = (props) =>
 {
-    const title = "Iron Flame (The Empyrean, 2)";
-    const author = "Rebecca Yarros";
+    console.log(props);
+    
     return(
         <article className='book'>
-            <img src="https://images-na.ssl-images-amazon.com/images/I/A1FW1G46+aL._AC_UL600_SR600,400_.jpg" alt="Iron Flame (The Empyrean, 2)" />
+            <img src={img} alt={title} />
 
             <h2>{title}</h2>
 
             <h4>{author}</h4>
+
+            <p>{props.job}</p>
+            <p>{props.title}</p>
+            
         </article>
     );
 };
