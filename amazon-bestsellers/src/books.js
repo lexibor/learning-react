3fe77de005df87f670a613da4674411425b82688
lexibor <brionesornelas.alexia@gmail.com>
2023-09-12 12:57:41 -0500

    images in src folder

--- amazon-bestsellers/src/books.js
+++ amazon-bestsellers/src/books.js
@@ -1,15 +1,26 @@
+import img1 from './images/book-1.jpg'
+import img2 from './images/book-2.jpg'
+import img3 from './images/book-3.jpg'
+
 export const books = [
     {
         author: 'Rebecca Yarros',
         title: 'Iron Flame (The Empyrean, 2)',
-        img: './images/book-1.jpg',
+        img: img1,
         id: 1
     },
 
     {
         author: 'Keila Shaheen',
         title: 'The Shadow Work Journal',
-        img: './images/book-2.jpg',
+        img: img2,
+        id: 2
+    },
+
+    {
+        author: 'J.R.R Tolkien',
+        title: 'The Lord of the Rings Illustrated',
+        img: img3,
         id: 2
     }
 ];
