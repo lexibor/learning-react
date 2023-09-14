a842b507a1579275c83d41ccdff432d984924fcd
lexibor <brionesornelas.alexia@gmail.com>
2023-09-14 13:29:37 -0500

    use state hook to update a number incrementing button

--- 03-advanced-react/src/tutorial/01-useState/starter/02-useState-basics.jsx
+++ 03-advanced-react/src/tutorial/01-useState/starter/02-useState-basics.jsx
@@ -1,5 +1,23 @@
-const UseStateBasics = () => {
-  return <h2>useState basics</h2>;
+import { useState } from "react";
+
+const UseStateBasics = () => 
+{
+
+  // console.log(useState());
+
+  const [count, setCount] = useState(0);
+
+  const handleClick = () =>
+  {
+    setCount(count + 1)
+  }
+
+  return(
+    <div>
+      <h4> You clicked {count} times</h4>
+      <button type="button" className="btn" onClick={handleClick}>click me</button>
+    </div>
+  );
 };
 
 export default UseStateBasics;
