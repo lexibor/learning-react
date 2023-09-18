e6439abbae8592df7bb5e8fce72e1f4308b71e86
lexibor <brionesornelas.alexia@gmail.com>
2023-09-18 15:14:15 -0500

    usestate with set timeout and current value

--- 03-advanced-react/src/tutorial/01-useState/starter/05-useState-gotcha.jsx
+++ 03-advanced-react/src/tutorial/01-useState/starter/05-useState-gotcha.jsx
@@ -1,5 +1,26 @@
+import { useState } from "react";
+
 const UseStateGotcha = () => {
-  return <h2>useState "gotcha"</h2>;
+  const [count, setCount] = useState(0)
+
+  const handleClick = () =>
+  {
+    setTimeout(() =>
+    {
+        setCount((currentState) =>
+        {
+          return currentState + 1;
+        });
+    }, 3000);
+  }
+
+  return (
+    <div>
+       <h1>{count}</h1>
+       <button type="button" className="btn" style={{marginTop: '2rem'}} onClick={handleClick}>add</button>
+    </div>
+ 
+  );
 };
 
 export default UseStateGotcha;
