f173e40f36a2e0373fc4f9df935a3a2460c3c2b6
lexibor <brionesornelas.alexia@gmail.com>
2023-09-19 11:13:52 -0500

    fetch with useEffect

--- 03-advanced-react/src/App.jsx
+++ 03-advanced-react/src/App.jsx
@@ -1,4 +1,4 @@
-import Starter from './tutorial/02-useEffect/starter/03-multiple-effects'
+import Starter from './tutorial/02-useEffect/starter/04-fetch-data'
 
 import Final from './tutorial/01-useState/final/05-useState-gotcha'
 
