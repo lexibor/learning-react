f3df9a999873af898208fbe24ee15e3f981c2228
lexibor <brionesornelas.alexia@gmail.com>
2023-09-20 16:13:30 -0500

    multiple returns

--- 03-advanced-react/src/App.jsx
+++ 03-advanced-react/src/App.jsx
@@ -1,6 +1,6 @@
-import Starter from './tutorial/02-useEffect/starter/04-fetch-data'
+import Starter from './tutorial/03-conditional-rendering/starter/01-multiple-returns-basics'
 
-import Final from './tutorial/01-useState/final/05-useState-gotcha'
+import Final from './tutorial/03-conditional-rendering/final/01-multiple-returns-basics'
 
 function App() {
   return (
