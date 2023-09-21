90f7d8ee615215fe219f35a83eca97a39032fa49
lexibor <brionesornelas.alexia@gmail.com>
2023-09-21 11:39:03 -0500

    fetching data and multiple returns

--- 03-advanced-react/src/App.jsx
+++ 03-advanced-react/src/App.jsx
@@ -1,4 +1,4 @@
-import Starter from './tutorial/03-conditional-rendering/starter/01-multiple-returns-basics'
+import Starter from './tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data'
 
 import Final from './tutorial/03-conditional-rendering/final/01-multiple-returns-basics'
 
--- 03-advanced-react/src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
+++ 03-advanced-react/src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
@@ -1,7 +1,60 @@
 import { useEffect, useState } from 'react';
 const url = 'https://api.github.com/users/QuincyLarson';
 
-const MultipleReturnsFetchData = () => {
-  return <h2>Fetch Data </h2>;
+const MultipleReturnsFetchData = () => 
+{
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+  const [user, setUser] = useState(null);
+
+
+  useEffect(() =>
+  {
+      const fetchUser = async () =>
+      {
+
+        try 
+        {
+          const response = await fetch(url);
+          if(!response.ok)
+          {
+            setIsError(true);
+            setIsLoading(false);
+            return 
+          }
+          const data = await response.json();
+
+          setUser(data);
+        } 
+        catch (error) 
+        {
+          setIsError(true);
+          console.log(error);
+        }
+        setIsLoading(false);
+      }
+
+      fetchUser();
+  }, []);
+
+
+  if(isLoading)
+  {
+    return <h2>Loading...</h2>
+  }
+
+  if(isError)
+  {
+    return <h2>there was an error</h2>
+  }
+
+  return (
+    <div>
+      <img style={{width: '150px', borderRadius: '25px'}} src={user.avatar_url} alt={user.name} />
+      <h2>{user.name}</h2>
+      <h4>Works at: {user.company}</h4>
+      <p>{user.bio}</p>
+    </div>
+  );
 };
 export default MultipleReturnsFetchData;
