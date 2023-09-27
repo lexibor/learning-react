1548c181d8bcf1e3d86452c01dab5a364afd4edc
lexibor <brionesornelas.alexia@gmail.com>
2023-09-27 13:02:34 -0500

    truthy and falsy values

--- 03-advanced-react/src/tutorial/03-conditional-rendering/starter/04-short-circuit-overview.jsx
+++ 03-advanced-react/src/tutorial/03-conditional-rendering/starter/04-short-circuit-overview.jsx
@@ -1,6 +1,22 @@
 import { useState } from 'react';
 
-const ShortCircuitOverview = () => {
-  return <h2>short circuit overview</h2>;
+const ShortCircuitOverview = () => 
+{
+
+  const [text, setText] = useState('');
+  const [name, setName] = useState('Susan');
+
+const codeExample = text || 'hello world'
+
+
+  return (
+    <div>
+      <h4>Falsy OR : {text || 'Hello World'}</h4>
+      <h4>Falsy AND : {text && 'Hello World'}</h4>
+      <h4>Truthy OR : {name || 'Hello World'}</h4>
+      <h4>Truthy AND : {name && 'Hello World'}</h4>
+      {codeExample}
+    </div>
+  );
 };
 export default ShortCircuitOverview;
