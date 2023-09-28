e6d15f13f6564033929931905de99a4b9d3a6aff
lexibor <brionesornelas.alexia@gmail.com>
2023-09-28 11:02:52 -0500

    if useEffect is triggered conditionally (a toggle for example) it will run every time even if it is set on initial render

--- 03-advanced-react/src/App.jsx
+++ 03-advanced-react/src/App.jsx
@@ -1,4 +1,4 @@
-import Starter from './tutorial/03-conditional-rendering/starter/07-user-challenge'
+import Starter from './tutorial/02-useEffect/starter/05-cleanup-function'
 
 import Final from './tutorial/03-conditional-rendering/final/01-multiple-returns-basics'
 
--- 03-advanced-react/src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
+++ 03-advanced-react/src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
@@ -1,5 +1,29 @@
+import { useEffect, useState } from "react";
+
 const CleanupFunction = () => {
-  return <h2>cleanup function</h2>;
+
+  const [toggle, setToggle] = useState(false);
+
+  //console.log(toggle);
+
+  return (
+    <div>
+      <button className="btn" onClick={() => setToggle(!toggle)}>toggle</button>
+      {toggle ? <RandomComponent /> : null}
+    </div>
+  );
 };
+
+const RandomComponent = () =>
+{
+  useEffect(() => 
+  {
+    console.log('interesting');
+  } , [])
+
+  return (
+    <h1>Hello there!</h1>
+  )
+}
 
 export default CleanupFunction;
