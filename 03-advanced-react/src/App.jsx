8f6bb9c7b11e3ddd75a3e78d638650e3c3f7b83d
lexibor <brionesornelas.alexia@gmail.com>
2023-09-29 10:29:51 -0500

    importing multiple named exports

--- 03-advanced-react/src/App.jsx
+++ 03-advanced-react/src/App.jsx
@@ -1,13 +1,15 @@
 // import Starter from './tutorial/02-useEffect/starter/05-cleanup-function'
 // import Final from './tutorial/03-conditional-rendering/final/01-multiple-returns-basics'
-import Navbar from "./tutorial/04-project-structure/starter/Navbar";
+
+import {Home, About} from "./tutorial/04-project-structure/starter/Pages";
 
 
 
 function App() {
   return (
     <div className='container'>
-      <Navbar />
+      <Home />
+      <About />
     </div>
   );
 }
