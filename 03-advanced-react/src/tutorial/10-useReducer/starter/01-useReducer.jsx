c7bcf99ec18fd3355aa2f70cd9819bc560147b2c
lexibor <brionesornelas.alexia@gmail.com>
2023-10-26 12:12:15 -0500

    separating components needed for useReducer in separate files for readability

--- 03-advanced-react/src/tutorial/10-useReducer/starter/01-useReducer.jsx
+++ 03-advanced-react/src/tutorial/10-useReducer/starter/01-useReducer.jsx
@@ -1,40 +1,19 @@
 import {useState, useReducer} from 'react';
 import { data } from '../../../data';
 
+import { CLEAR_LIST, RESET_LIST, REMOVE_ITEM } from './actions';
+import reducer from './reducer';
+
+
 const defaultState = 
 {
   people: data,
 
 }
 
-const CLEAR_LIST = 'CLEAR_LIST';
-const RESET_LIST = 'RESET_LIST';
-const REMOVE_ITEM = 'REMOVE_ITEM';
 
-const reducer = (state, action) =>
-{
-  //state is state before the object
 
-  if(action.type === CLEAR_LIST)
-  {
-      return {...state, people: []}
-  }
 
-  if(action.type === RESET_LIST)
-  {
-    return {...state, people: data}
-  }
-
-  if(action.type === REMOVE_ITEM)
-  {
-    let newPeople = state.people.filter((person) => person.id !== action.payload.id);
-
-    return {...state, people: newPeople};
-  }
-
-  throw new Error(`No matching "${action.type}" - action type`);
-  // console.log(action);
-};
 
 const ReducerBasics = () => {
 
---
+++ 03-advanced-react/src/tutorial/10-useReducer/starter/actions.js
@@ -0,0 +1,6 @@
+
+
+
+export const CLEAR_LIST = 'CLEAR_LIST';
+export const RESET_LIST = 'RESET_LIST';
+export const REMOVE_ITEM = 'REMOVE_ITEM';
