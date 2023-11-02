5091a653fe9f1a880b232cf54535de5c227793ca
lexibor <brionesornelas.alexia@gmail.com>
2023-11-02 12:14:56 -0500

    using React.memo to prevent unessesary rerenders

--- 03-advanced-react/src/App.jsx
+++ 03-advanced-react/src/App.jsx
@@ -1,4 +1,4 @@
-import Starter from './tutorial/11-performance/starter/02-lower-state-challenge'
+import Starter from './tutorial/11-performance/starter/03-hooks'
 //import Final from './tutorial/09-context-api/final/context'
 
 
